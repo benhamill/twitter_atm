16e706bd84de2acbe97a17cb91ecc2b5403f5544
Ben Hamill <git-commits@benhamill.com>
2011-01-13 01:42:54 -0600

    Prep for interactive command.

--- lib/twitter_atm/client.rb
+++ lib/twitter_atm/client.rb
@@ -1,6 +1,19 @@
 class TwitterAtm::Client
-  def self.pin_url(consumer_key, consumer_secret)
-    client = TwitterOAuth::Client.new(:consumer_key => consumer_key, :consumer_secret => consumer_secret)
-    client.request_token.authorize_url
+  attr_accessor :client, :request_token
+
+  def initialize consumer_key, consumer_secret
+    @client = TwitterOAuth::Client.new(:consumer_key => consumer_key, :consumer_secret => consumer_secret)
+  end
+
+  def authorize_url
+    @request_token = client.request_token
+    request_token.authorize_url
+  end
+
+  def authorize pin
+    client.authorize request_token.token, request_token.secret, :pin => pin.to_i
+    access_token = client.send :access_token
+
+    [access_token.token, access_token.secret]
   end
 end
